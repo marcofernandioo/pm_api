878906bf44dc12dca57ed3e3811b23036b601f93
Marco Fernandio <marcofernandioo@gmail.com>
2021-01-20 01:09:23 +0700

    Some additional features

--- models/pricelist.js
+++ models/pricelist.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var pricelistSchema = new Schema({
     category: String,
     name: String,
+    qty: Number,
     price: Number
 })
 
--- routes/pricelist.js
+++ routes/pricelist.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 var Pricelist = require('../models/pricelist');
 
+// Add a product to the Pricelist
 router.post('/add', (req,res) => {
     if (req.body && req.body.name && req.body.category && req.body.price){
         var new_product = new Pricelist({
             name: req.body.name, 
             category: req.body.category,
+            qty: 0, 
             price: req.body.price
         });
         new_product.save((err) => {
@@ -22,24 +24,35 @@ router.get('/get', (req,res) => {
     Pricelist.find((err,list) => {
         if (!err) {
             var pricelist = list;
-            res.json({pricelist})
+            res.json({status: 'ok', list: pricelist})
         }
         else res.json({status: 'err', msg: 'Coba ulang kembali'})
     })
 })
 
 //Update pricelist 
-router.all('/update/:id', (req,res) => {
-    if (req.body && req.body.price) {
+router.post('/update', (req,res) => {
+    if (req.body && req.body.id) {
         let update = {};
+        if (req.body.price) update.price = req.body.price;
+        if (req.body.name) update.name = req.body.name;
+        if (req.body.category) update.category = req.body.category;
         update.price = req.body.price;
-        Pricelist.findByIdAndUpdate(req.params.id, update,(err,prod) => {
-            if (!err){
-                res.json({status: 'ok', msg: 'Produk telah di update'})
-            } else res.json({status: 'err', msg: 'Coba ulang kembali'})
+        Pricelist.findByIdAndUpdate(req.body.id, update, (err) => {
+            if (!err) res.json({status: 'ok', msg: 'Produk telah diubah'})
+            else res.json({status: 'err', msg: 'Coba ulang kembali'})
         })
     } else res.json({status: 'err', msg: 'Masukkan harga produk'});
     
 })
+
+router.get('/find/:id', (req,res) => {
+    Pricelist.findById(req.params.id, (err,product) => {
+        if (!err) res.json({status: 'ok', product});
+        else res.json({status: 'err', msg: err})
+    })
+})
+
+
 
 module.exports = router;
