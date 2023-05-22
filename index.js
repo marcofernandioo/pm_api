6d2cb43941237443fd6b5b8e83f2d1b3ee7b503a
marco <marcofernandioo@gmail.com>
2023-05-22 22:33:58 +0800

    Small changes to redeploy the API

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const orderRouter = require('./routes/order');
 const pricelistRouter = require('./routes/pricelist');
 const salesRouter = require('./routes/sales');
 
-const PORT = process.env.PORT || 8008;
+const PORT = process.env.PORT || 8000;
 var app = express();
 
 app.use(cors({credentials: true, origin: process.env.CLIENT}));
@@ -19,16 +19,9 @@ app.use(express.json());
 app.use('/order', orderRouter);
 app.use('/pricelist', pricelistRouter);
 app.use('/sales', salesRouter);
-app.use('/uwu', (req,res) => res.json({status: 'ok', msg: 'uwu kekw'}))
 app.use('/', (req,res) => res.send('Hello There...'))
 
 
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log(`listenin' to port ${PORT}, database connected`)))
-.catch((err) => console.log('error: ' + err.message))
-// var db = mongoose.connection;
-// db.on('error', () => console.log.bind(console, 'connection err'));
-// db.once('open', () => console.log('DB Connected'));
-
-
-// app.listen(PORT, () => console.log(`listenin' to post ${PORT}`))
+.then(() => app.listen(PORT, () => console.log(`Listenin' to port ${PORT}, database connected`)))
+.catch((err) => console.log('ERROR: ' + err))
